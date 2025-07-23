491f359a938467df60bf24494df9eebb4c5cb5f9
Your Name <you@your-domain.com>
2025-07-23 16:37:05 +0900

    feat: Relationships

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address, Country
 
 # Register your models here.
 
@@ -9,4 +9,9 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "rating", )
     list_display =("title", "author", )
 
+    
+    
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
