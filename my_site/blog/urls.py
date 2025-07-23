1a386dcba95eb48e1241605781b3398255d71f49
Your Name <you@your-domain.com>
2025-07-23 16:36:34 +0900

    feat: Building a Blog - Data & Models (Minjoo)

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -11,8 +11,4 @@ urlpatterns = [
 
 """
 Q. Slug?
-여기서 <slug:slug>는 동적 경로(dynamic segment)를 의미함. 
-사용자가 /posts/my-first-post 같은 주소에 접근하면,
-ex) "my-first-post"가 slug라는 이름의 변수로 추출되어
-views.post_detail() 함수에 인자로 전달됩니다.
 """
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,93 +1,106 @@
 from datetime import date
 from django.shortcuts import render
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+from django.shortcuts import get_object_or_404
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+from .models import Post
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#  all_posts = [
+#     {
+#         "slug": "hike-in-the-mountains",
+#         "image": "mountains.jpg",
+#         "author": "Maximilian",
+#         "date": date(2021, 7, 21),
+#         "title": "Mountain Hiking",
+#         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     },
+#     {
+#         "slug": "programming-is-fun",
+#         "image": "coding.jpg",
+#         "author": "Maximilian",
+#         "date": date(2022, 3, 10),
+#         "title": "Programming Is Great!",
+#         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     },
+#     {
+#         "slug": "into-the-woods",
+#         "image": "woods.jpg",
+#         "author": "Maximilian",
+#         "date": date(2020, 8, 5),
+#         "title": "Nature At Its Best",
+#         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-def get_date(post):
-    return post['date']
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     }
+# ]
+
+# def get_date(post):
+#     post = Post.objects.all()
+#     return post.date
 
 def starting_point (request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    # all_posts = Post.objects.all()
+    # sorted_posts = sorted(all_posts, key=all_posts)
+    # latest_posts = sorted_posts[-3:]
+    
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 def posts(request):
     return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts 
+        "all_posts": Post.objects.all()
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # all_posts = Post.objects.all()
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # return render(request, "blog/post-detail.html", {
+    #     "post": identified_post
+    # })
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post
     })
+    
 
 """
 URLConf(urls.py)에서 받은 slug 문자열이 post_detail() 함수의 두 번째 인자로 들어옴.
-이 값을 이용해서 보통은 데이터베이스에서 해당 slug에 해당하는 포스트를 찾음. 
 """
