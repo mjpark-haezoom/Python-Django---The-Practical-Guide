79611e2089b4e3cc21fc90754ed94b078960e3b8
Your Name <you@your-domain.com>
2025-07-25 16:13:27 +0900

    feat: Section11 Class Views (Done)

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,27 +1,47 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
-class ReviewView(View):
-    def get(self, request): 
-        form = ReviewForm()
-        
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-    
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
+
 
 # def review(request):
 #     if request.method == 'POST':
@@ -45,3 +65,5 @@ def thank_you(request):
 #         "form": form
 #     })
 
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
