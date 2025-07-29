ac397cdf2408247cef36c192b5610b62d932eaa7
Your Name <you@your-domain.com>
2025-07-30 08:43:08 +0900

    feat: Section 14 - Course Project

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
+from .views import StartingPageView, AllPostsView, SinglePostView, ReadLaterView
 
 urlpatterns = [
-    path("", views.starting_point, name="starting-page"), 
-    path("posts", views.posts, name="posts-page"), 
-    path("posts/<slug:slug>", views.post_detail, 
-         name="post-detail-page") # dynamic segment (angle bracket), parameter name -> /posts/my-first-post
-] 
+    path("", views.StartingPageView.as_view(), name="starting-page"), 
+    path("posts", views.AllPostsView.as_view(), name="posts-page"), 
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"), # dynamic segment (angle bracket), parameter name -> /posts/my-first-post
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 """
 Q. Slug?
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,108 +1,161 @@
 from datetime import date
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
+from .forms import CommentForm
 
-#  all_posts = [
-#     {
-#         "slug": "hike-in-the-mountains",
-#         "image": "mountains.jpg",
-#         "author": "Maximilian",
-#         "date": date(2021, 7, 21),
-#         "title": "Mountain Hiking",
-#         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+# 3개의 뷰
 
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     },
-#     {
-#         "slug": "programming-is-fun",
-#         "image": "coding.jpg",
-#         "author": "Maximilian",
-#         "date": date(2022, 3, 10),
-#         "title": "Programming Is Great!",
-#         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+class StartingPageView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     },
-#     {
-#         "slug": "into-the-woods",
-#         "image": "woods.jpg",
-#         "author": "Maximilian",
-#         "date": date(2020, 8, 5),
-#         "title": "Nature At Its Best",
-#         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-#         "content": """
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+class AllPostsView(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
+    context_object_name = "all_posts"
+    ordering = ["-date"]
 
-#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-#         """
-#     }
-# ]
 
-# def get_date(post):
-#     post = Post.objects.all()
-#     return post.date
-
-def starting_point (request):
-    # all_posts = Post.objects.all()
-    # sorted_posts = sorted(all_posts, key=all_posts)
-    # latest_posts = sorted_posts[-3:]
+class SinglePostView(View):
+    template_name = "blog/post-detail.html"
+    model = Post
     
-    latest_posts = Post.objects.all().order_by("-date")[:3] 
-    # SQL 구문으로 바꿔서 모든 데이터를 가져오지 않고 일부 부분만 가져옴. 장고에서 -index는 지원하지 않음. 
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+        return is_saved_for_later
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        stored_posts = request.session.get("stored_posts")
+        
+        context = {
+            "post": post,
+            "post_tag": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
+        }
+        return render(request, "blog/post-detail.html", context)
 
-def post_detail(request, slug):
-    # all_posts = Post.objects.all()
-    # identified_post = next(post for post in all_posts if post['slug'] == slug)
-    # return render(request, "blog/post-detail.html", {
-    #     "post": identified_post
-    # })
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags" : identified_post.tags.all()
-    })
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+        
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+            
+        return render(request, "blog/stored-posts.html", context)
     
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+        request.session["stored_posts"] = stored_posts # 저장
+        return HttpResponseRedirect("/")
+
+
+# def starting_point (request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     # SQL 구문으로 바꿔서 모든 데이터를 가져오지 않고 일부 부분만 가져옴. 장고에서 -index는 지원하지 않음.
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts
+#     })
+
+# def post_detail(request, slug):
+#     identified_post = get_object_or_404(Post, slug=slug)
+
+#     if request.method == 'POST':
+#         comment_form = CommentForm(request.POST)
+#         if comment_form.is_valid():
+#             comment = comment_form.save(commit=False)
+#             comment.post = identified_post
+#             comment.save()
+#             return redirect("post-detail-page", slug=slug)
+
+#     else:
+#         comment_form = CommentForm()
+
+#     return render(request, "blog/post-detail.html", {
+#                 "post": identified_post,
+#                 "post_tags" : identified_post.tags.all(),
+#                 "comments" : identified_post.comments.all(), # 연결된 댓글들
+#                 "comment_form": comment_form
+#             })
+
+# def read_later(request):
+#     stored_posts = request.session.get("stored_posts") # 저장된 게시물 id 리스트
+
+#     context = {}
+
+#     if stored_posts is None or len(stored_posts) == 0:
+#         context["posts"] = []
+#         context["has_posts"] = False
+#     else:
+#         posts = Post.objects.filter(id__in=stored_posts) # id를 기준으로 Post 객체를 불러옴
+#         context["posts"] = posts
+#         context["has_posts"] = True
+
+#     return render(request, "blog/stored-posts.html", context)
 
 """
 URLConf(urls.py)에서 받은 slug 문자열이 post_detail() 함수의 두 번째 인자로 들어옴.
